3902ca2e25b3b3692f03c0e8c197080bdb74fbaa
Rafael Bitencourt <Rafael Bitencourt@10.1.48.61>
2015-11-06 21:54:13 -0200

    Inserido Tabela Informação

---
+++ src/main/java/br/com/imobiliaria/model/Informacao.java
@@ -0,0 +1,78 @@
+package br.com.imobiliaria.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "INFORMACAO")
+public class Informacao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long ID;
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+    @Column(name = "telefone", nullable = false, length = 20)
+    private String telefone;
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+    @Column(name = "descricao", nullable = false, length = 400)
+    private String descricao;
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="idimovel")
+    private Imovel imovel;
+
+    public Informacao() {
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+}
