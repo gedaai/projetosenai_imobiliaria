89432beeb88e4a0292972e5800e8ae7ddd5052a8
Rafael Bitencourt <Rafael Bitencourt@10.1.48.61>
2015-11-07 09:14:10 -0200

    Inserido Tabela Informação

--- src/main/java/br/com/imobiliaria/rest/ImovelResourse.java
+++ src/main/java/br/com/imobiliaria/rest/ImovelResourse.java
@@ -3,16 +3,21 @@ package br.com.imobiliaria.rest;
 import br.com.imobiliaria.dao.ImovelDAO;
 import br.com.imobiliaria.model.Imovel;
 import java.util.List;
+import java.util.Objects;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("imoveis")
@@ -38,5 +43,25 @@ public class ImovelResourse {
     @Path("{id}")
     public void delete(@PathParam("id") Long id){
         imovelDAO.excluir(id);
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Imovel update(@PathParam("id") Long id, Imovel imovel){
+        if(!Objects.equals(id, imovel.getId())){
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        return imovelDAO.atualizar(imovel);
+    }
+    
+    @GET
+    @Path("{id}")
+    public Response find(@PathParam("id") Long id) {
+        final Imovel imovel = imovelDAO.buscar(id);
+        if (imovel == null) {
+            throw new EntityNotFoundException();
+        }
+        return Response.ok(imovel).build();
     }
 }
