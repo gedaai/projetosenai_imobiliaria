7fc48e652f3f0aaf1f2e7844a40f2bbf00109290
Rafael Bitencourt <Rafael Bitencourt@Rafael.mshome.net>
2015-11-13 21:37:36 -0200

    Finalizando Login

--- src/main/webapp/js/loginModalFunc.js
+++ src/main/webapp/js/loginModalFunc.js
@@ -1,20 +1,23 @@
 angular.module('imobiliaria').controller('loginModalFunc', function ($scope, Corretor, $modalInstance) {
-   
-   $scope.corretorLogado = {nome: "TESTE"}  
+     
    $scope.login = function (corretor) {
     console.log("LOGIN");
     console.log(corretor);
     new Corretor(corretor).create()
           .then(function (data) {
-            console.log(data);
-            $scope.corretorLogado = data;
-            console.log($scope.corretorLogado);
+            corretor = data;
+            console.log(corretor);
+            if(corretor.id){
+                $scope.erro = null;
+                $modalInstance.close(corretor);      
+            }else{
+                $scope.erro = "erro";
+            }
         }, function (error) {
             console.log('error', error);
             alert(error.data);
     });
-    
-    $modalInstance.close(corretor);
+ 
   };
 
   $scope.cancel = function () {
